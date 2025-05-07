1a5c3b02e7831def26d6e5bcd1e81f731de60849
Michael0623Wan <wanchunho56@gmail.com>
2025-05-07 20:01:00 +0800

    v2

--- myscript.js
+++ myscript.js
@@ -10,7 +10,7 @@ function AddElement(){
         li.className = 'list-group-item list-group-item-action d-flex justify-content-between align-items-center text-break';
         li.innerHTML = 
         `<button onclick="done()" class="btn btn-outline-warning done-btn"><i class="fa-solid fa-check"></i></button>
-        ${Input.value}
+        <span>${Input.value}</span>
         <button onclick="remove()" class="btn btn-outline-dark delete-btn"><i class="fa-regular fa-trash-can"></i></button>`;
         List.appendChild(li);
     }
@@ -23,29 +23,39 @@ List.addEventListener('click', ClickHandler);
 
 function ClickHandler(e){
     if(e.target.classList.contains('delete-btn')){
-        Remove(e);
+        e.target.parentElement.remove();
+        SaveLocal();
     } else if (e.target.classList.contains('done-btn')){
-        Done(e);
+        e.target.parentElement.classList.toggle("text-decoration-line-through");
+        e.target.parentElement.classList.toggle("list-group-item-success");
+        SaveLocal();
     } else {
-        Edit(e);
+        const Item = e.target.closest('.list-group-item');
+        if(!Item || Current) return;
+
+        Current = Item;
+        const Text = Item.querySelector("span");
+        const OriginText = Text.textContent;
+
+        const InputElement = document.createElement('input');
+        InputElement.type = 'text';
+        InputElement.className = 'form-control'; 
+        InputElement.maxlength = '30';
+        InputElement.value = OriginText;
+
+        // Replace the text with the input field
+        Text.innerHTML = '';
+        Text.appendChild(InputElement);
+        InputElement.focus();
+
+        // Handle saving changes
+        InputElement.addEventListener('blur', function() {
+            const taskTextSpan = listItem.querySelector('span');
+            taskTextSpan.innerHTML = InputElement.value;
+            Current = null;
+            savelocal();
+        });
     }
-}
-
-
-function Done(e){
-    e.target.parentElement.classList.toggle("text-decoration-line-through");
-    e.target.parentElement.classList.toggle("list-group-item-success");
-    SaveLocal();
-}
-
-function Remove(e){
-    e.target.parentElement.remove();
-    SaveLocal();
-}
-
-function Edit(e){
-    const Item = e.target.closest('.list-group-item');
-
 }
 
 function SaveLocal(){
